6a9d1e65f1df3e2ddda947b95af9126a9261ebb1
Venius24 <WonderRebirth@mail.ru>
2025-11-27 18:34:59 +0500

    Birthday date calculations

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 
+from .utils import calculate_birthday_countdown
+
+from .forms import BirthdayForm
+
 
 def birthday(request):
-    context = {}
-    return render(request, 'birthday/birthday.html', context=context)
+    form = BirthdayForm(request.GET or None)
+    context = {'form': form}
+    if form.is_valid():
+        birthday_countdown = calculate_birthday_countdown(
+            # ...и передаём в неё дату из словаря cleaned_data.
+            form.cleaned_data['birthday']
+        )
+        # Обновляем словарь контекста: добавляем в него новый элемент.
+        context.update({'birthday_countdown': birthday_countdown})
+    return render(request, 'birthday/birthday.html', context) 
