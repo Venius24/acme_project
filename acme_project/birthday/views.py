34049bf2df0a7ed2972145569e4c834dd5d3924b
Venius24 <WonderRebirth@mail.ru>
2025-12-03 14:57:25 +0500

    birthday list and csrf

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -4,11 +4,14 @@ from .utils import calculate_birthday_countdown
 
 from .forms import BirthdayForm
 
+from .models import Birthday
+
 
 def birthday(request):
-    form = BirthdayForm(request.GET or None)
+    form = BirthdayForm(request.POST or None)
     context = {'form': form}
     if form.is_valid():
+        form.save()
         birthday_countdown = calculate_birthday_countdown(
             # ...и передаём в неё дату из словаря cleaned_data.
             form.cleaned_data['birthday']
@@ -16,3 +19,10 @@ def birthday(request):
         # Обновляем словарь контекста: добавляем в него новый элемент.
         context.update({'birthday_countdown': birthday_countdown})
     return render(request, 'birthday/birthday.html', context) 
+
+def birthday_list(request):
+    # Получаем все объекты модели Birthday из БД.
+    birthdays = Birthday.objects.all()
+    # Передаём их в контекст шаблона.
+    context = {'birthdays': birthdays}
+    return render(request, 'birthday/birthday_list.html', context) 
