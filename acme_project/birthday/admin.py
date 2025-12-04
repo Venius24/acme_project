da8180e5bcde5bc3c1995933453210b6e5f0ad48
Venius24 <WonderRebirth@mail.ru>
2025-12-04 15:27:28 +0500

    admin and cleaning

--- acme_project/birthday/admin.py
+++ acme_project/birthday/admin.py
@@ -1,1 +1,21 @@
 from django.contrib import admin
+
+from .models import Birthday
+
+admin.site.empty_value_display = 'Не задано'
+
+class BirthdayAdmin(admin.ModelAdmin):
+    list_display = (
+        'first_name',
+        'last_name',
+        'birthday',
+    )
+    list_display_links = ('first_name',)
+    list_editable = (
+        'last_name',
+        'birthday',
+    )
+    
+# Регистрируем класс с настройками админки для моделей IceCream и Category:
+admin.site.register(Birthday, BirthdayAdmin)
+
